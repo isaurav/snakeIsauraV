1d9afea0bb2b4d9b93c62ab8689843d28043407a
isaura ventura <isaura.ventura@yahoo.com>
2014-10-07 15:01:09 -0700

    setting background

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -2,4 +2,23 @@ var snake;
 
 var context;
 var screenWidth;
-var screenHight;
+var screenHeight;
+
+function gameInitialize() {
+    var canvas = document.getElemntById ("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+}   screenHeight = window.innerHeight;
+
+    canvas.width = screenWidth;
+    canvas.Height = screenHeight;
+} 
+
+function gameLoop () {  
+}
+
+function gameDraw () {
+    context.fillStyle = "rgb(180,250,213)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
