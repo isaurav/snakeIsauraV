d3c137133a67593f21b58151b098889b1ccf0b07
isaura ventura <isaura.ventura@yahoo.com>
2014-10-14 14:56:29 -0700

    building my snake

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,24 +1,50 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var context;
 var screenWidth;
 var screenHeight;
 
+gameInitialize();
+gameDraw();
+
 function gameInitialize() {
-    var canvas = document.getElemntById ("game-screen");
+    var canvas = document.getElementById ("game-screen");
     context = canvas.getContext("2d");
     
     screenWidth = window.innerWidth;
-}   screenHeight = window.innerHeight;
+    screenHeight = window.innerHeight;
 
     canvas.width = screenWidth;
-    canvas.Height = screenHeight;
-} 
+    canvas.height = screenHeight;
+    } 
 
-function gameLoop () {  
+function gameLoop() {  
 }
 
-function gameDraw () {
-    context.fillStyle = "rgb(180,250,213)";
+function gameDraw() {
+    context.fillStyle = "rgb(180, 250, 213)";
     context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitilaize() {
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 20;
+    
+    for(var index = 0; index < snakeLength; index++) {
+        snake.push( {
+            x: index,
+            y: 0
+        });
+    }
+}
+
+function snakeDraw() {
+    
+}
+
+function snakeUpdate() {
+    
 }
